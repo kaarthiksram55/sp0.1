58b85d7293189e67e29ede0fc6b10f13f4199491
kaarthiksram55 <38404733+kaarthiksram55@users.noreply.github.com>
2018-05-17 22:31:14 +0530

    Create sorting.cpp

---
+++ sorting.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int a[10] = {3,9,16,2,4,18,5,99,7,17};
+    int i,k;
+    for(i=0;i<10;i++){
+        for(k=i+1;k<10;k++){
+            if(a[k]<a[i]){
+                a[i]=a[i]+a[k];
+                a[k]=a[i]-a[k];
+                a[i]=a[i]-a[k];
+            }
+        }
+    }
+    return 0;
+}
